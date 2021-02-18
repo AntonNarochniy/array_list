5830ef06eff66595d09ac7ce637d9484bb7b5435
Anton <test.788test788@gmail.com>
2021-02-18 20:43:31 +0600

    Добавил комментарии

--- src/kg/megacom/Main.java
+++ src/kg/megacom/Main.java
@@ -6,14 +6,16 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList< String> books = new ArrayList<>(Arrays.asList("Курочка ряба", "Каштанка", "Колобок", "Буратино", "Мойдодыр"));
-        System.out.println(books.toString());
-        String temp = books.get(books.size()-1);
-        books.remove(books.size()-1);
-        System.out.println(books.toString());
-        books.set(books.size()-1, temp);
-        System.out.println(books.get(books.size() - 1));
-        books.clear();
-        books.addAll(Arrays.asList("Java. Библиотека профессионала – Кей С. Хорстманн", "Head First", "Паттерны проектирования – Эрик Фримен, Элизабет Робсон", "Spring в действии – Крейг Уоллс" ));
+        ArrayList< String> books = new ArrayList<>(Arrays.asList("Курочка ряба", "Каштанка", "Колобок", "Буратино", "Мойдодыр")); //  добавить 5 книг
+        System.out.println(books.toString());  // распечатать
+        String temp = books.get(books.size()-1); //сохранить последнюю книгу
+        books.remove(books.size()-1); //удалить последнюю книгу
+        System.out.println(books.toString());  //распечатать
+        books.set(books.size()-1, temp); //заменить последнюю книгу удаленной
+        System.out.println(books.get(books.size() - 1)); //распечатать только последнюю книгу
+        books.clear(); //очистить список
+        books.addAll(Arrays.asList("Java. Библиотека профессионала – Кей С. Хорстманн", "Head First",
+                "Паттерны проектирования – Эрик Фримен, Элизабет Робсон",
+                "Spring в действии – Крейг Уоллс" )); //добавить 3 книги которые вы планируете почитать
     }
 }
